aecfafe1fe8bfdf2a1dc5a18bf475b18225cc480
adiha <adi.hayon1@gmail.com>
2017-10-19 13:12:21 +0300

    Add files via upload

--- Netlist_to_Z3_latency_nor2.py
+++ Netlist_to_Z3_latency_nor2.py
@@ -1,16 +1,20 @@
 import re
 import pdb
+import logicPaths
 
-def readoperations(data, varLegendRow, varLegendCol):
+def readoperations(data, varLegendRow, varLegendCol, OutputString):
     
     # last row is gnd, for conversion of NOT to NOR
     D = []
     types = []
-    for i in xrange(len(varLegendCol)+1):
+    
+    # dictionary which its keys are gates and its values are circuit outputs
+    circuit_output_gates = {}
+    for i in range(len(varLegendCol)+1):
         types.append(0)
-    for i in xrange(len(varLegendRow)+1):
+    for i in range(len(varLegendRow)+1):
         l = []
-        for j in xrange(len(varLegendCol)):
+        for j in range(len(varLegendCol)):
             l.append(0)
         D.append(l)
     occurences = re.findall("inv1.*?\(\.a\((.*?)\).*?\.O\((.*?)\)\);", data, re.DOTALL)
@@ -23,7 +27,12 @@ def readoperations(data, varLegendRow, varLegendCol):
         outIdx = varLegendCol.index(out)
         D[in1Idx][outIdx] = 1
         D[len(varLegendRow)][outIdx] = 1 # gnd => 1 every column of a NOT gate
-        types[outIdx] = 1    
+        types[outIdx] = 1
+
+        # Check if this gate is a circuit output, and if so add it to the circuit_output_gates dictionary.
+        if out in OutputString:
+            circuit_output_gates[outIdx] = out
+            
     occurences = re.findall("nor2.*?\(\.a\((.*?)\).*?\.b\((.*?)\).*?\.O\((.*?)\)\);", data, re.DOTALL)
     for occur in occurences:
         in1 = occur[0].replace(" ","")
@@ -37,6 +46,11 @@ def readoperations(data, varLegendRow, varLegendCol):
         D[in1Idx][outIdx] = 1
         D[in2Idx][outIdx] = 1
         types[outIdx] = 2    
+        
+        # Check if this gate is a circuit output, and if so add it to the circuit_output_gates dictionary.
+        if out in OutputString:
+            circuit_output_gates[outIdx] = out
+        
     occurences = re.findall("nor3.*?\(\.a\((.*?)\).*?\.b\((.*?)\).*?\.O\((.*?)\)\);", data, re.DOTALL)
     for occur in occurences:
         in1 = occur[0].replace(" ","")
@@ -52,15 +66,23 @@ def readoperations(data, varLegendRow, varLegendCol):
         D[in1Idx][outIdx] = 1
         D[in2Idx][outIdx] = 1
         D[in3Idx][outIdx] = 1
-        types[outIdx] = 3    
-    return D, types
+        types[outIdx] = 3
+
+        # Check if this gate is a circuit output, and if so add it to the circuit_output_gates dictionary.
+        if out in OutputString:
+            circuit_output_gates[outIdx] = out
         
-def generate_Z3_file(name, D, types, InputString, numrows=1000, numcols=1000):
+    return D, types, circuit_output_gates
+
+
+def generate_Z3_file(name, D, types, InputString, OutputString, circuit_output_gates, numrows=1000, numcols=1000):
+    
+    # Get all logic paths
+    paths = logicPaths.get_all_paths_wrapper(D)
     
     # Get needed data from files
     optfilename = name + '_python_Z3input.txt'
-    print optfilename
-    opt = file(optfilename,'wb')
+    opt = open(optfilename,'w')
     
     Dval  = D
     r = len(Dval)
@@ -82,7 +104,7 @@ def generate_Z3_file(name, D, types, InputString, numrows=1000, numcols=1000):
     opt.write('\n')
     opt.write(';## Define variables\n')
     
-    for i in xrange(1, gate_num + 1):
+    for i in range(1, gate_num + 1):
         opt.write('(declare-const g%d_Rin1 Int)\n'% i)
         opt.write('(declare-const g%d_Cin1 Int)\n'% i)
         if types[i-1] == 2:
@@ -91,19 +113,23 @@ def generate_Z3_file(name, D, types, InputString, numrows=1000, numcols=1000):
         opt.write('(declare-const g%d_Rout Int)\n'% i)
         opt.write('(declare-const g%d_Cout Int)\n'% i)
         opt.write('(declare-const g%d_T Int)\n' % i)
-    for i in xrange(len(InputString)):
+    for i in range(len(InputString)):
         opt.write('(declare-const %s_Row Int)\n'% InputString[i])
         opt.write('(declare-const %s_Col Int)\n'% InputString[i])
+    for i in range(len(OutputString)):
+        opt.write('(declare-const %s_Row Int)\n'% OutputString[i])
+        opt.write('(declare-const %s_Col Int)\n'% OutputString[i])
         
     opt.write('(declare-const Latency Int)\n')
     opt.write('\n')
     opt.write(';## Constraints\n')
     opt.write(';# Bounds\n')
     
-    for i in xrange(1, gate_num + 1):
+    for i in range(1, gate_num + 1):
         opt.write('(assert (> g%d_T 0))\n' % i)
+        opt.write('(assert (<= g%d_T %d))\n' % (i, gate_num))
 
-    for i in xrange(1, gate_num + 1):
+    for i in range(1, gate_num + 1):
         opt.write('(assert (and (> g%d_Rin1 0) (<= g%d_Rin1 ROWNUM)))\n' % (i,i))
         opt.write('(assert (and (> g%d_Cin1 0) (<= g%d_Cin1 COLNUM)))\n' % (i,i))
         if types[i-1] == 2:
@@ -111,13 +137,14 @@ def generate_Z3_file(name, D, types, InputString, numrows=1000, numcols=1000):
             opt.write('(assert (and (> g%d_Cin2 0) (<= g%d_Cin2 COLNUM)))\n' % (i,i))
         opt.write('(assert (and (> g%d_Rout 0) (<= g%d_Rout ROWNUM)))\n' % (i,i))
         opt.write('(assert (and (> g%d_Cout 0) (<= g%d_Cout COLNUM)))\n' % (i,i))
-    for i in xrange(1, len(InputString) + 1):
+    for i in range(1, len(InputString) + 1):
         opt.write('(assert (and (> %s_Row 0) (<= %s_Row ROWNUM)))\n' % (InputString[i-1],InputString[i-1]))
         opt.write('(assert (and (> %s_Col 0) (<= %s_Col COLNUM)))\n' % (InputString[i-1],InputString[i-1]))
+    
     opt.write('\n');
 
     opt.write(';# Unique rows or columns\n');
-    for i in xrange(1, gate_num + 1):
+    for i in range(1, gate_num + 1):
         if types[i-1] == 1:
             opt.write('(assert (or\n')
             opt.write('    (and (= g%d_Rin1 g%d_Rout) (not(= g%d_Cin1 g%d_Cout))) ;Either rows are equal and columns are different\n' % (i,i,i,i))
@@ -133,9 +160,14 @@ def generate_Z3_file(name, D, types, InputString, numrows=1000, numcols=1000):
     
     opt.write('\n');
     opt.write(';# Parallel operation of NOR2 gates\n');
-    for i in xrange(1, gate_num + 1):
-        for j in xrange(i+1, gate_num + 1):
-            if types[i-1] == types[j-1] and types[i-1] == 2: # If both gates are NOR2
+    for i in range(1, gate_num + 1):
+        for j in range(i+1, gate_num + 1):
+            inSamePath = False
+            for path in paths:
+                if i in path and j in path:
+                    inSamePath = True
+                    break
+            if (not inSamePath) and (types[i-1] == types[j-1] and types[i-1] == 2): # If both gates are NOR2
                 opt.write('(assert\n')
                 opt.write('  (or (not(= g%d_T g%d_T))\n' %  (i,j))
                 opt.write('    (and (or (and (= g%d_Cin1 g%d_Cin1) (= g%d_Cin2 g%d_Cin2)) (and (= g%d_Cin1 g%d_Cin2) (= g%d_Cin2 g%d_Cin1))) (= g%d_Cout g%d_Cout) (and (= g%d_Rin1 g%d_Rin2 g%d_Rout) (= g%d_Rin1 g%d_Rin2 g%d_Rout)))\n' % (i,j,i,j,i,j,i,j,i,j,i,i,i,j,j,j))   
@@ -145,9 +177,14 @@ def generate_Z3_file(name, D, types, InputString, numrows=1000, numcols=1000):
     opt.write('\n')
     
     opt.write(';# Parallel operation of NOT gates\n');
-    for i in xrange(1, gate_num + 1):
-        for j in xrange(i+1, gate_num + 1):
-            if types[i-1] == types[j-1] and types[i-1] == 1: # If both gates are NOT
+    for i in range(1, gate_num + 1):
+        for j in range(i+1, gate_num + 1):
+            inSamePath = False
+            for path in paths:
+                if i in path and j in path:
+                    inSamePath = True
+                    break
+            if (not inSamePath) and (types[i-1] == types[j-1] and types[i-1] == 1): # If both gates are NOT
                 opt.write('(assert\n')
                 opt.write('  (or (not(= g%d_T g%d_T))\n' %  (i,j))
                 opt.write('    (and (= g%d_Cin1 g%d_Cin1) (= g%d_Cout g%d_Cout) (= g%d_Rin1 g%d_Rout) (= g%d_Rin1 g%d_Rout))\n' % (i,j,i,j,i,i,j,j))
@@ -157,44 +194,51 @@ def generate_Z3_file(name, D, types, InputString, numrows=1000, numcols=1000):
     opt.write('\n')
     
     opt.write(';# NOT and NOR2 gates cannot operate in the same clock cycle\n')
-    for i in xrange(1, gate_num + 1):
-        for j in xrange(i+1, gate_num + 1):
+    for i in range(1, gate_num + 1):
+        for j in range(i+1, gate_num + 1):
             if not (types[i-1] == types[j-1]): # If one of the gates is NOT and the other is NOR2
                 opt.write('(assert (not (= g%d_T g%d_T)))\n' % (i,j))
     opt.write('\n')
     
     opt.write(';# All outputs are different\n')
-    for i in xrange(1, gate_num + 1):
-        for j in xrange(i+1, gate_num + 1):
+    for i in range(1, gate_num + 1):
+        for j in range(i+1, gate_num + 1):
             opt.write('(assert (or (not(= g%d_Cout g%d_Cout)) (not(= g%d_Rout g%d_Rout))))\n' % (i,j,i,j))
     opt.write('\n')
     
     opt.write(';# Conectivity constraints\n')
-    for i in xrange(1, gate_num + 1):
+    for i in range(1, gate_num + 1):
         inputs = [gate_rows.index(k)+1 for k in gate_rows if Dval[k][i - 1] == 1] 
-        for j in xrange(len(inputs)): # No protection against loops. Netlist must be valid.
+        for j in range(len(inputs)): # No protection against loops. Netlist must be valid.
             opt.write('(assert (= g%d_Rout g%d_Rin%d))\n' % (inputs[j],i,j+1))
             opt.write('(assert (= g%d_Cout g%d_Cin%d))\n' % (inputs[j],i,j+1))
             opt.write('(assert (< g%d_T g%d_T))\n' % (inputs[j],i))
         circuit_inputs =  [input_rows.index(k)+1 for k in input_rows if Dval[k][i-1] == 1] 
-        for j in xrange(len(circuit_inputs)): # No protection against loops. Netlist must be valid.
+        for j in range(len(circuit_inputs)): # No protection against loops. Netlist must be valid.
             opt.write('(assert (= %s_Row g%d_Rin%d))\n' % (InputString[circuit_inputs[j]-1],i,j+len(inputs)+1))
             opt.write('(assert (= %s_Col g%d_Cin%d))\n' % (InputString[circuit_inputs[j]-1],i,j+len(inputs)+1))
     opt.write('\n')
     
     opt.write(';# Circuit inputs constraints\n')
-    for i in xrange(1, gate_num + 1):
-        for j in xrange(len(InputString)):
+    for i in range(1, gate_num + 1):
+        for j in range(len(InputString)):
             opt.write('(assert (not (and (= %s_Row g%d_Rout) (= %s_Col g%d_Cout))))\n' % (InputString[j], i, InputString[j], i))
-    for i in xrange(1, len(InputString) + 1):
-        for j in xrange(i + 1, len(InputString)):
+    for i in range(1, len(InputString) + 1):
+        for j in range(i + 1, len(InputString)):
             opt.write('(assert (not (and (= %s_Row %s_Row) (= %s_Col %s_Col))))\n' % (InputString[i], InputString[j], InputString[i], InputString[j]))
     opt.write('\n')
     
     opt.write(';# Latency constraints\n')
-    for i in xrange(1, gate_num + 1):
+    for i in range(1, gate_num + 1):
         opt.write('(assert (>= Latency  g%d_T))\n' % i)
-
+    opt.write('\n')
+    
+    opt.write(';# Circuit outputs Constraints\n')
+    for i in range(1, gate_num + 1):
+        
+        if i in circuit_output_gates:
+            opt.write('(assert (= g%d_Rout %s_Row))\n' % (i, circuit_output_gates[i]))
+            opt.write('(assert (= g%d_Cout %s_Col))\n' % (i, circuit_output_gates[i]))
     opt.write('\n')
     opt.write(';## Objective function\n')
     opt.write('(minimize(+ Latency 0))\n')
@@ -205,8 +249,8 @@ def generate_Z3_file(name, D, types, InputString, numrows=1000, numcols=1000):
     
 def netlist_to_z3(filename):
     BenchmarkStrings = [filename]
-    for ii in xrange(len(BenchmarkStrings)):
-        bmfId = file(BenchmarkStrings[ii]) # open file
+    for ii in range(len(BenchmarkStrings)):
+        bmfId = open(BenchmarkStrings[ii]) # open file
         data = bmfId.read()
         
         # read input/output/wire
@@ -222,10 +266,11 @@ def netlist_to_z3(filename):
         varLegendRow = InputString + WireString + OutputString
     
         # analyze dependencies
-        D, types = readoperations(data,varLegendRow,varLegendCol)
+        D, types, circuit_output_gates = readoperations(data,varLegendRow,varLegendCol, OutputString)
+        
         bmfId.close() # close file
         
         r = len(varLegendRow)+1
         c = varLegendCol
         
-        return generate_Z3_file(BenchmarkStrings[ii], D, types, InputString, r, r)
+        return generate_Z3_file(BenchmarkStrings[ii], D, types, InputString, OutputString, circuit_output_gates, r, r)
